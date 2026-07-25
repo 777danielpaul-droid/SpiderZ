62ee68c9c0edf2c47d2ef75b76a76c9fd9829e87
Daniel Paul <777danielpaul@gmail.com>
2026-07-25 01:37:43 -0700

    ci: GitHub Pages Deploy via Actions + base /SpiderZ/

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// base = "/SpiderZ/" damit Assets unter github.io/SpiderZ/assets/ laden (Sub-Pfad-Deploy)
 export default defineConfig({
+  base: '/SpiderZ/',
   plugins: [react()],
 })
